0e4d29498ac38510f7e9ac7130c01e12cc383065
Aaron Hernandez <aypierre223@gmail.com>
2024-11-18 19:47:29 -0600

    FuncionalLogin

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,68 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home.js';
-import Login from './Login.js';
-import Signup from './Signup.js';
-import OptionsPage from './OptionsPage.js';
-import ProfileOptionsPage from './ProfileOptionsPage.js';
-import EditProfilePage from './EditProfilePage.js';
-import EmployeeListPage from './EmployeeListPage.js';
-import JobOffersPage from './JobOffersPage.js';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.js";
+import PrivateRoute from "./components/PrivateRoute.js";
+import Home from "./Home.js";
+import Login from "./components/Login.jsx";
+import Signup from "./Signup.js";
+import OptionsPage from "./OptionsPage.js";
+import ProfileOptionsPage from "./ProfileOptionsPage.js";
+import EditProfilePage from "./EditProfilePage.js";
+import EmployeeListPage from "./EmployeeListPage.js";
+import JobOffersPage from "./JobOffersPage.js";
 
-
-function MainApp() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/options" element={<OptionsPage />} />
-                <Route path="/hacer-perfil" element={<ProfileOptionsPage />} />
-                <Route path="/make-edit-profile" element={<EditProfilePage/>} />
-                <Route path="/buscar-empleado" element={<EmployeeListPage />} />
-                <Route path="/look-at-offers" element={<JobOffersPage />}/>
-            </Routes>
-        </Router>
-    );
+function App() {
+	return (
+		<AuthProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route
+						path="/options"
+						element={
+							<PrivateRoute>
+								<OptionsPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/hacer-perfil"
+						element={
+							<PrivateRoute>
+								<ProfileOptionsPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/make-edit-profile"
+						element={
+							<PrivateRoute>
+								<EditProfilePage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/buscar-empleado"
+						element={
+							<PrivateRoute>
+								<EmployeeListPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/look-at-offers"
+						element={
+							<PrivateRoute>
+								<JobOffersPage />
+							</PrivateRoute>
+						}
+					/>
+				</Routes>
+			</Router>
+		</AuthProvider>
+	);
 }
 
-
-export default MainApp;
+export default App;
